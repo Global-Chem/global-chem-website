05e8c093e9db025f34313357f9f4a4351ed4ccb1
gerrardsarah <gerrardsarah@gmail.com>
2023-04-24 12:50:13 -0700

    removed something from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,5 @@ module.exports = {
       },
     },
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
   plugins: [],
 }
