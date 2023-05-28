b560a56c889d78324f45e1ea088111673f668cb1
Sul <sharifsuliman1@gmail.com>
2023-05-28 10:36:43 -0400

    Update next.config.js
    Signed-off-by: Sul  <sharifsuliman1@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/' : '',
+//   assetPrefix: isProd ? '/' : '',
   images: {
     unoptimized: true,
   },
